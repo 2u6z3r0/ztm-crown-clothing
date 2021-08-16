638547e6d849e54d1efbf46d36657843de44b6b4
Vitthal <samrat.patil0202@gmail.com>
2021-08-16 21:32:55 +0530

    created homepage with smaller components and finished styling menu-item component with animation

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import Directory from "components/directory/directory.component";
+import React from "react";
+import "./homepage.styles.scss";
+
+function HomePage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
+
+export default HomePage;
